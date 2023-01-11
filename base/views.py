b3318f1039848389f8943e639513d8e1d1f67aa6
mezeiser <mezardini@gmail.com>
2023-01-11 10:24:32 +0100

    changed the templates

--- base/views.py
+++ base/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'streams.html')
+
+def quiz(request):
+    return render(request, 'room.html')
